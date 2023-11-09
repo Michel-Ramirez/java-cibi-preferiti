209e20d4ee60434e8d94d6ed6f3415a0f45ee8b6
Michel-Ramirez <michel.ramirez19@gmail.com>
2023-11-09 17:11:55 +0100

    first commit

---
+++ src/org/lesson/java/CibiPreferiti.java
@@ -0,0 +1,14 @@
+package org.lesson.java;
+
+public class CibiPreferiti {
+
+	public static void main(String[] args) {
+		String[] favoriteFoods = {"CARBONARA", "PIZZA", "HAMBURGER", "PASTA AL PESTO", "ARROSTO", "SPEZZATINO"};
+		
+		System.out.println(favoriteFoods.length);
+		System.out.println(favoriteFoods[0]);
+		System.out.println(favoriteFoods[5]);
+
+	}
+
+}
